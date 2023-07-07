1cc9b493fe95c64fd32650d884e8f7aae6827c13
saegii <saegjj18@gmail.com>
2023-07-08 00:44:53 +0200

    change folder structure because of corrupt folder

---
+++ client-side/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client-side/src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  
+  email = '';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  signUp() {
+    console.log('Signing up with email:', this.email);
+    localStorage.setItem('isLoggedIn', 'true');
+  }
+
+  signIn() {
+    console.log('Signing in with email:', this.email);
+    localStorage.setItem('isLoggedIn', 'true');
+  }
+
+}
