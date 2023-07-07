4a08c70f38399ec17b1fb0a6fb04e5799be013e0
saegii <saegjj18@gmail.com>
2023-07-07 22:27:42 +0200

    init spring boot backend

---
+++ camparranger-backend/src/test/java/com/camparranger/camparrangerbackend/CamparrangerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camparranger.camparrangerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamparrangerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
