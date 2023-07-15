e900b0e51f3ac6c8391eaf598093d4d0abc8192b
saegii <saegjj18@gmail.com>
2023-07-15 11:55:26 +0200

    move mapper for user and auth package dependency

--- server-side/src/main/java/com/saegiiproject/camparranger/authentication/UserMapper.java
+++ server-side/src/main/java/com/saegiiproject/camparranger/authentication/UserMapper.java
@@ -1,6 +1,7 @@
-package com.saegiiproject.camparranger.user;
+package com.saegiiproject.camparranger.authentication;
 
 import com.saegiiproject.camparranger.authentication.LoginRequest;
+import com.saegiiproject.camparranger.user.User;
 import org.springframework.stereotype.Component;
 
 @Component
